75e119d145b5472121fd7f12474e13d2304eda41
agustinasc <agustina.santillancastro@gmail.com>
2021-02-04 00:51:39 -0300

    Terminado con css y comenzando con js

---
+++ main.js
@@ -0,0 +1,37 @@
+// SIDEBAR
+
+///////////// DECLARACION DE CONST Y LETS /////////
+const bigAside = document.getElementById('big-aside');
+const navImagen = document.getElementById('nav-imagen');
+const navTexto = document.getElementById('nav-texto');
+const asideTexto = document.getElementById('aside-texto');
+const asideImagen = document.getElementById('aside-imagen');
+const asideImagenClose = document.getElementById('aside-imagen-close');
+const asideTextoClose = document.getElementById('aside-texto-close');
+
+
+///////// DECLARACION DE EVENTOS //////////////////
+
+navImagen.addEventListener('click', ()=>{
+    ImageBitmapRenderingContext.a
+    asideImagen.style.display = 'block';
+    asideTexto.style.display ='none';
+})
+navTexto.addEventListener('click', ()=>{
+    asideTexto.style.display ='block';
+    asideImagen.style.display = 'none'
+})
+
+asideImagenClose.addEventListener('click', ()=>{
+    asideImagen.style.display = 'none'
+    //alert('funciona')
+})
+asideTextoClose.addEventListener('click', ()=>{
+    asideTexto.style.display = 'none'
+})
+// .big-aside{
+//     display: none;
+// }
+// .first-section{
+//     width: 100%;
+// }
