bd67d9f6b9c97fdead9183f8599abdaadb56034b
agustinasc <agustina.santillancastro@gmail.com>
2021-02-07 17:21:14 -0300

    Subiendo nuevas funcionalidades

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // SIDEBAR
 
-///////////// DECLARACION DE CONST Y LETS /////////
+///////////// DECLARACION DE VARIABLES /////////
 
 const bigAside = document.getElementById('big-aside');
 const firstSection = document.getElementById('first-section');
@@ -22,19 +22,41 @@ const bottomText = document.getElementById('bottom-text');
 const bottomTitle = document.getElementById('bottom-title')
 const textFont = document.getElementById('text-font');
 const textFontSize = document.getElementById('text-font-size');
+
 const btnLeft = document.getElementById('btn-left');
 const btnCenter = document.getElementById('btn-center');
 const btnRight = document.getElementById('btn-right');
-
+const btnNinguno = document.getElementById('btn-ninguno');
+const btnClaro = document.getElementById('btn-claro');
+const btnOscuro = document.getElementById('btn-oscuro');
 
 const colores = document.getElementById('colores');
 const valorTexto = document.getElementById('valor-texto');
 const coloresFondo = document.getElementById('background-text');
 const valorFondo = document.getElementById('valor-fondo');
+const colorBackg = document.getElementById('color-background');
+let textoBackg = document.getElementById('background-texto');
 
 const transparente = document.getElementById('transparente');
 
-///////// DECLARACION DE EVENTOS //////////////////
+const spaceText = document.getElementById('space-text');
+const interlineText = document.getElementById('interline-text');
+
+const fondoSelect = document.getElementById('fondo-select');
+
+const filterBri = document.getElementById('filter-brightness');
+const filterOpa = document.getElementById('filter-opacity');
+const filterCon = document.getElementById('filter-contrast');
+const filterBlu = document.getElementById('filter-blur');
+const filterGra = document.getElementById('filter-grayscale');
+const filterSep = document.getElementById('filter-sepia');
+const filterHue = document.getElementById('filter-hue');
+const filterSat = document.getElementById('filter-saturation');
+const filterNeg = document.getElementById('filter-invert');
+const btnImagenReset = document.getElementById('btn-imagen-reset')
+
+/////////  EVENTOS ///////////////
+
 /////////////EVENTOS DEL NAV /////////
 
 navImagen.addEventListener('click', ()=>{
@@ -57,18 +79,83 @@ asideTextoClose.addEventListener('click', ()=>{
     asideTexto.style.display = 'none'
 })
 
-//////////EVENTOS DE IMAGEN ////////////
-urlImagen.addEventListener('change', ()=>{
-    let imagen = document.createElement('img');
-    memeImg.appendChild(imagen);
-    let src = urlImagen.value;
-    imagen.setAttribute('src', src);
-    imagen.classList.add('meme-img')
-    //imagen.setAttribute('width', 400);
 
+//////////EVENTOS DE SIDEBAR - IMAGEN ////////////
+
+
+urlImagen.addEventListener('change', (e)=>{
+    let src = urlImagen.value;
+    console.log(src)
+    memeImg.style.backgroundImage = `url('${src}')`;
+    memeImg.style.backgroundPosition = 'center';
+    memeImg.style.backgroundRepeat = 'no-repeat';
+    //memeImg.style.backgroundSize = 'contain';
+})
+colorBackg.addEventListener('input', (e)=>{
+    memeImg.style.backgroundColor =  e.target.value;
+    textoBackg.innerHTML =  e.target.value.toUpperCase();
 })
 
+fondoSelect.addEventListener('change', ()=>{
+    if (fondoSelect.value == 'aclarar'){
+        memeImg.classList.add('aclarar')
+    } else if (fondoSelect.value == 'oscurecer'){
+        memeImg.classList.add('oscurecer')
+    } else if (fondoSelect.value == 'diferencia'){
+        memeImg.classList.add('diferencia')
+    } else if (fondoSelect.value == 'luminosidad'){
+        memeImg.classList.add('luminosidad')
+    } else if (fondoSelect.value =='multiplicar'){
+        memeImg.classList.add('multiplicar')
+    }
+})
+
+filterBri.addEventListener('change', (e)=>{
+   let valueFilter = e.target.value
+    memeImg.style.filter = `brightness(${valueFilter})`;
+})
+filterOpa.addEventListener('change', (e)=>{
+   let valueFilter = e.target.value
+    memeImg.style.filter = `opacity(${valueFilter})`;
+})
+filterCon.addEventListener('change', (e)=>{
+   let valueFilter = e.target.value
+    memeImg.style.filter = `contrast(${valueFilter}%)`;
+})
+// filterBlu.addEventListener('change', (e)=>{
+//    let valueFilter = e.target.value
+//    console.log((valueFilter/10))
+//     memeImg.style.filter = `blur(${valueFilter}px)`;
+// })
+filterGra.addEventListener('change', (e)=>{
+   let valueFilter = e.target.value
+    memeImg.style.filter = `grayscale(${valueFilter}%)`;
+})
+filterSep.addEventListener('change', (e)=>{
+   let valueFilter = e.target.value
+    memeImg.style.filter = `sepia(${valueFilter}%)`;
+})
+// filterHue.addEventListener('change', (e)=>{
+//     let valueFilter = e.target.value;
+//     console.log(valueFilter);
+//     memeImg.style.filter = `hue-rotation(${valueFilter}deg)`;
+// })
+// filterSat.addEventListener('change', (e)=>{
+//     let valueFilter = e.target.value
+//     memeImg.style.filter = `saturation(${valueFilter}%)`;
+// })
+filterNeg.addEventListener('change', (e)=>{
+    let valueFilter = e.target.value
+    memeImg.style.filter = `invert(${valueFilter})`;
+})
+btnImagenReset.addEventListener('click', ()=>{
+    
+})
+
+
+
 /////////// EVENTOS DE TEXTO ////////////////
+
 
 topText.addEventListener('keyup', ()=>{
     let newTopText = topText.value;
@@ -102,7 +189,7 @@ bottomTextCheck.addEventListener('click', ()=>{
 
 textFont.addEventListener('change', ()=>{
    topTitle.style.fontFamily = `${textFont.value}`;
-   bottomTitle.style.fontFamily = `${textFont.value}`
+   bottomTitle.style.fontFamily = `${textFont.value}`;
 })
 
 textFontSize.addEventListener('change', ()=>{
@@ -130,7 +217,7 @@ btnRight.addEventListener('click', (e)=>{
     bottomTitle.style.justifyContent = 'flex-end'
 })
 
-//Eventos de colores//
+//////COLORES///////
 
 colores.addEventListener('input', (e)=> {
     console.log(e.target.value);
@@ -141,9 +228,9 @@ colores.addEventListener('input', (e)=> {
     //console.log(valorColores.value)
 })
 
-//Evento de fondos //
+///////////FONDO///////////
+
 coloresFondo.addEventListener('input', (e)=>{
-    console.log(e.target.value);
     topTitle.style.backgroundColor = e.target.value;
     bottomTitle.style.backgroundColor = e.target.value;
     valorFondo.innerHTML = e.target.value.toUpperCase();
@@ -156,4 +243,38 @@ transparente.addEventListener('click', (e)=>{
     }else if (e.target.checked == false) {
         //HAY QUE ACHICAR LA FOTO
     }
+})
+
+//////////BOTONES DE CONTORNO ///////
+
+btnNinguno.addEventListener('click', (e)=>{
+    e.preventDefault();
+    topTitle.style.textShadow = 'none';
+    bottomTitle.style.textShadow = 'none';
+})
+btnClaro.addEventListener('click', (e)=>{
+    e.preventDefault();
+    topTitle.style.textShadow = 'rgb(255 255 255) 2px 2px, rgb(255 255 255) -2px 2px, rgb(255 255 255) 2px -2px, rgb(255 255 255) -2px -2px';
+    bottomTitle.style.textShadow = 'rgb(255 255 255) 2px 2px, rgb(255 255 255) -2px 2px, rgb(255 255 255) 2px -2px, rgb(255 255 255) -2px -2px';
+})
+btnOscuro.addEventListener('click', (e)=>{
+    e.preventDefault();
+    topTitle.style.textShadow = 'rgb(0 0 0) 2px 2px, rgb(0 0 0) -2px 2px, rgb(0 0 0) 2px -2px, rgb(0 0 0) -2px -2px';
+    bottomTitle.style.textShadow = 'rgb(0 0 0) 2px 2px, rgb(0 0 0) -2px 2px, rgb(0 0 0) 2px -2px, rgb(0 0 0) -2px -2px';
+})
+
+///// ESPACIADO /////
+
+spaceText.addEventListener('change', (e)=>{
+    let spaceValue = e.target.value;
+    topTitle.style.padding = `${spaceValue}px`;
+    bottomTitle.style.padding = `${spaceValue}px`;
+})
+
+///// INTERLINEADO /////
+
+interlineText.addEventListener('change', (e)=>{
+    let interlineValue = e.target.value;
+    topTitle.style.lineHeight = `${interlineValue}`;
+    bottomTitle.style.lineHeight = `${interlineValue}`;
 })
